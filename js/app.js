c7452e392e68827391fd8cb142a21f9d326f6b87
vascode <vascodepssn@gmail.com>
2016-03-05 15:02:10 -0500

    load map, add marker with infowindow from static arrary

--- js/app.js
+++ js/app.js
@@ -1,105 +1,205 @@
-var marker;
+// Model for places
+// TODO : this will be replaced by a function for getting data from fourSqure
+var places= [
+	{
+		'name': "Bar Louie",
+		'lat': 41.50094,
+		'lng': -81.69586,
+		'desc': "Restaurant, Bar, and Cocktail Bar · Downtown Cleveland"
+	},
+	{
+		'name': "House of Blues",
+		'lat': 41.49917,
+		'lng': -81.69066,
+		'desc': 'Music Venue and Bar · Downtown Cleveland'
+	},
+	{
+		'name': "West Side Market",
+		'lat': 41.48469,
+		'lng': -81.70306,
+		'desc': 'Market · Ohio City'
+	}
+];
 
-// function initMap () {
-function initMap(){
-	// var mapDiv = document.getElementById('map');
-	var mapDiv = document.querySelector('#map');
-	//this js can be replaced with jQuery
-	// var mapDiv = $("#map")[0];
 
-	// required parameters: center and zoom
-	var cleveland = new google.maps.LatLng(41.493378, -81.700712);
-	var mapOptions = {
-		zoom: 15,
-		center: cleveland,
-		disableDefaultUI: true,
-		mapTypeId: google.maps.MapTypeId.TERRAIN
+function ViewModel(){
+	var self = this;
+	var marker;
+	var markerArray = places;
+	var infoWindow = new google.maps.InfoWindow();
+	var map
+
+		// function initMap () {
+	self.initMap = function(){
+		// var mapDiv = document.getElementById('map');
+		var mapDiv = document.querySelector('#map');
+		//this js can be replaced with jQuery
+		// var mapDiv = $("#map")[0];
+
+		// required parameters: center and zoom
+		var cleveland = new google.maps.LatLng(41.493378, -81.700712);
+		var mapOptions = {
+			zoom: 15,
+			center: cleveland,
+			disableDefaultUI: true,
+			mapTypeId: google.maps.MapTypeId.TERRAIN
+		};
+
+		map = new google.maps.Map(mapDiv, mapOptions);
+
+		// TODO: adds search bars and list view onto map, sets styled map
 	};
 
-	var map = new google.maps.Map(mapDiv, mapOptions);
+		// this.toggleBounce = function() {
+	function toggleBounce(){
+		if (marker.getAnimation() !== null) {
+			marker.setAnimation(null);
+		} else {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+			setTimeout(function() {
+	    		marker.setAnimation(null);
+	    	}, 700);
+		}
+	}
 
-	marker = new google.maps.Marker({
-	    position: cleveland,
-	    map: map,
-	    // draggable: true,
-	    animation: google.maps.Animation.DROP,
-	    title: 'Hello World!'
-	  });
+	// this.getContentString = function(_place){
+	function getContentString(_place){
+		var contentString = '<div id="content">'+ '<div id="siteNotice">'+'</div>'+
+							'<h1 id="firstHeading" class="firstHeading">'+ _place.name +'</h1>'+
+							'<div id="bodyContent">'+
+							'<p><b>Interesting location description</b></p>'+
+							'<p>'+ _place.desc + '<p>' +
+							'</div>'+
+							'</div>';
+    	return contentString;
+	};
+		/*
+	* Function to create Info window for the google map marker
+	* Takes the marker data as a parameter
+	*/
+  	function createInfoWindow(mk, i){
+		/*
+		* Create the DOM element for the marker window
+		* Uses marker data to create Business name, phone number, reviewer's picture, and reviewer's review
+		*/
+		var infoWindowContent = '<div class="info_content">';
+		infoWindowContent += '<h4>' + markerArray[i].name + '</h4>';
+		infoWindowContent += '<p>' + markerArray[i].desc + '</p>';
 
-	var contentString = '<div id="content">'+
-	      '<div id="siteNotice">'+
-	      '</div>'+
-	      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-	      '<div id="bodyContent">'+
-	      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-	      'sandstone rock formation in the southern part of the '+
-	      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-	      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-	      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-	      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-	      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-	      'Aboriginal people of the area. It has many springs, waterholes, '+
-	      'rock caves and ancient paintings. Uluru is listed as a World '+
-	      'Heritage Site.</p>'+
-	      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-	      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-	      '(last visited June 22, 2009).</p>'+
-	      '</div>'+
-	      '</div>';
+					// var contentString = '<div id="content">'+ '<div id="siteNotice">'+'</div>'+
+					// 		'<h1 id="firstHeading" class="firstHeading">'+ markerArray[i].name +'</h1>'+
+					// 		'<div id="bodyContent">'+
+					// 		'<p><b>Interesting location description</b></p>'+
+					// 		'<p>'+ markerArray[i].desc + '<p>' +
+					// 		'</div>'+
+					// 		'</div>';
 
-	var infowindow = new google.maps.InfoWindow({
-	    content: contentString
-	});
+			// var infowindow = new google.maps.InfoWindow({
+			//     content: infoWindowContent
+			// });
 
-	// marker.addListener('click', toggleBounce);
-	marker.addListener('click', function(){
-		toggleBounce();
-    	infowindow.open(map, marker);
-	});
+
+		/*
+		* Google Map V3 method to set the content of the marker window
+		* Takes above infoWindowContent variable as a parameter
+		*/
+  		infoWindow.setContent(String(infoWindowContent));
+  		console.log(String(infoWindowContent));
+		/*
+		* Google Map V3 method to set the content of the marker window
+		* Takes map and marker data variable as a parameter
+		*/
+  		infoWindow.open(map, mk);
+  	}
+
+
+	self.addMarker = function(){
+
+		for (var i=0; i<markerArray.length; i++){
+			marker = new google.maps.Marker({
+			    position: new google.maps.LatLng(markerArray[i].lat, markerArray[i].lng),
+			    map: map,
+			    // draggable: true,
+			    animation: google.maps.Animation.DROP
+			});
+
+			// var contentString = getContentString(markerArray[i]);
+			// var contentString = '<div id="content">'+ '<div id="siteNotice">'+'</div>'+
+			// 				'<h1 id="firstHeading" class="firstHeading">'+ markerArray[i].name +'</h1>'+
+			// 				'<div id="bodyContent">'+
+			// 				'<p><b>Interesting location description</b></p>'+
+			// 				'<p>'+ markerArray[i].desc + '<p>' +
+			// 				'</div>'+
+			// 				'</div>';
+
+			// var infoWindow = new google.maps.InfoWindow({
+			//     content: contentString
+			// });
+			// var infoWindow = new google.maps.InfoWindow();
+
+			google.maps.event.addListener(marker, 'click', (function(mk, i){
+				return function(){
+		      		// infoWindow.open(map, mk);
+		      		createInfoWindow(mk, i);
+					toggleBounce();
+				}
+			})(marker, i));
+		}
+	}
+	/**
+ 	 * if this marker has no animation, disable other marker's animation
+ 	 * set this marker's animation to bounce
+	 * @param {Object} venueMarker A venue marker object
+ 	 * @return {void}
+ 	 */
+	function selectedMarkerBounce(venueMarker) {
+		// if this venue marker has no animation
+		if (venueMarker.getAnimation() == null) {
+			// set this marker as selected marker
+			self.selectedMarker(venueMarker);
+			// disable other venue's animation
+			self.topPicks().forEach(function(venue) {
+				venue.marker.setAnimation(null);
+			});
+			// set this marker's aniamtion to bounce
+			venueMarker.setAnimation(google.maps.Animation.BOUNCE);
+		}
+	}
+	self.initMap();
+	self.addMarker();
 };
 
-function toggleBounce() {
-	console.log('hi');
-	if (marker.getAnimation() !== null) {
-		marker.setAnimation(null);
-	} else {
-		marker.setAnimation(google.maps.Animation.BOUNCE);
-		setTimeout(function() {
-    		marker.setAnimation(null);
-    	}, 700);
-	}
+
+/*
+* @description callback function to initialize app when Google Maps Script finishes loading
+*/
+function startMap(){
+	// initMap(); // initialize the map
+
+	var viewModel = new ViewModel(); // define then bind viewModel
+	ko.applyBindings(viewModel);
+
+	// viewModel.initMap();
 }
 
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-// window.addEventListener('resize', function(e) {
-// 	//Make sure the map bounds get updated on page resize
-// 	map.fitBounds(window.mapBounds);
-// 	$('#map-canvas').height($(window).height());
-// });
 
-
-
-// initMap();
-
-
-// function initialize() {
-// 	/*
-// 	 * Set the variable for the starting point
-// 	 */
-//   var waikiki = new google.maps.LatLng(21.284712,  -157.805762);
-// 	/*
-// 	* Set the variable for the google map option
-// 	*/
-//   var mapOptions = {
-//     zoom: 14,
-//     center: waikiki,
-//     disableDefaultUI: true
-//   };
-// 	/*
-// 	* create a new map object
-// 	*/
-//   map = new google.maps.Map(document.getElementById('map'), mapOptions);
-// }
-
-// initialize();
+    // /**
+    //  * Takes a PlaceResult object and puts a marker on the map at its location.
+    //  * @param {Object} place A PlaceResult object returned from a Google Places
+    //  *   Library request.
+    //  * @return {Object} marker A Google Maps Marker objecte to be placed on the
+    //  *   map.
+    //  */
+    // function createMarker(place) {
+    //     var marker = new google.maps.Marker({
+    //         map: map,
+    //         position: place.geometry.location,
+    //     });
+    //     // When a marker is clicked scroll the corresponding list view element
+    //     // into view and click it.
+    //     google.maps.event.addListener(marker, 'click', function () {
+    //         document.getElementById(place.id).scrollIntoView();
+    //         $('#' + place.id).trigger('click');
+    //     });
+    //     return marker;
+    // }
